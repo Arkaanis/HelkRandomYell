3cc015e35a992bc6f56d759198fe2b2fdc28ddb6
Arkaanis <alex@nucu.eu>
2026-01-21 23:23:39 +0000

    Update HRYells with new quotes

--- HelkRandomYell_quotes.lua
+++ HelkRandomYell_quotes.lua
@@ -1,7 +1,11 @@
 HRYells = {
-  "Ring is loosly hung!",
+  "Stooop! I could've dropped my croissaaaant!",
+  "Do you have any idea who I think I am?!",
+  "FREE SHAVOCADOOS!!!",
   "HELKPO!!!!11",
   "WHO WILL TELL OF THE FROZEN PEOPLE?",
   "Oh I'd lava slice",
+  "Hold up - mmmm heeey",
   "Shalkar did this!"
+
 } 
