3e490faf1e70507ab379802b8ee20704bea9e417
Arkaanis <alex@nucu.eu>
2026-01-21 23:05:15 +0000

    Adding relevant files

---
+++ HelkRandomYell.lua
@@ -0,0 +1,44 @@
+local f = CreateFrame("Frame")
+f:RegisterEvent("ADDON_LOADED")
+f:SetScript("OnEvent", function(self, event, addon)
+    if event == "ADDON_LOADED" and addon == "HelkRandomYell" then
+        
+        -- check if we already made the button, if not make it now
+        if not RandomYellButton then
+            RandomYellButton = CreateFrame("Button", "RandomYellButton", UIParent, "UIPanelButtonTemplate")
+            RandomYellButton:SetSize(100, 30)
+            RandomYellButton:SetText("Yell Stuff!")
+            RandomYellButton:SetScript("OnClick", RandomYell_OnClick())
+        end
+
+        function RandomYell_OnClick()
+          if #HRYells > 0 then
+            local randIndex = math.random(#HRYells)
+            SendChatMessage(HRYells[randIndex], "Yell")
+          end
+        end
+        
+        -- make it moveable on left click
+        RandomYellButton:SetMovable(true)
+        RandomYellButton:EnableMouse(true)
+        RandomYellButton:RegisterForDrag("LeftButton")
+        RandomYellButton:SetScript("OnDragStart", RandomYellButton.StartMoving)
+        RandomYellButton:SetScript("OnDragStop", RandomYellButton.StopMovingOrSizing)
+        
+        -- if we moved it previously, we load the position here
+        if HRYButtonPosition.point then
+            RandomYellButton:ClearAllPoints()
+            RandomYellButton:SetPoint(HRYButtonPosition.point, UIParent, HRYButtonPosition.relPoint, HRYButtonPosition.ofsx, HRYButtonPosition.ofsy)
+        else
+            RandomYellButton:SetPoint("TOPLEFT", UIParent, 50, 150)
+        end
+        
+        self:UnregisterEvent("ADDON_LOADED")
+    elseif event == "PLAYER_LOGOUT" then
+        if RandomYellButton then
+            local point, relPoint, ofsx, ofsy = RandomYellButton:GetPoint(1)
+            HRYButtonPosition.point, HRYButtonPosition.relPoint, HRYButtonPosition.ofsx, HRYButtonPosition.ofsy = point, relPoint, ofsx, ofsy
+        end
+    end
+end)
+f:RegisterEvent("PLAYER_LOGOUT")
---
+++ HelkRandomYell_quotes.lua
@@ -0,0 +1,7 @@
+HRYells = {
+  "Ring is loosly hung!",
+  "HELKPO!!!!11",
+  "WHO WILL TELL OF THE FROZEN PEOPLE?",
+  "Oh I'd lava slice",
+  "Shalkar did this!"
+} 
